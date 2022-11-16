adb7540cc4958becc56504755d4d4a73057eba7f
Aditya Syam <97302238+adityasyam@users.noreply.github.com>
2022-11-16 17:22:37 -0500

    Update HardToWriteFunctions.py

--- HardToWriteFunctions.py
+++ HardToWriteFunctions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math 
 
 
 def getPlayableActions(currentState, differentials, timestep):
@@ -10,6 +11,33 @@ def getPlayableActions(currentState, differentials, timestep):
     acceleration_power = 1  # m/s/s
     braking_power = 1  # m/s/s
     max_turning_rate = 45  # deg/s
+    
+    #calculating max velocity reachable
+    max_vel = currentState[2] + acceleration_power*(timestep)
+
+    #calculating min velocity reachable
+    min_vel = currentState[2] - braking_power*(timestep) 
+
+    #calculating max clockwise angle reachable
+    clock_max_angle = (currentState[3]-(max_turning_rate*timestep))%360
+
+    #calculating max counter-clockwise angle reachable
+    counter_max_angle = (currentState[3]+(max_turning_rate*timestep))%360
+
+    #min_x_coordinate
+    init_x_coord = currentState[0]
+
+    #min_y_coordinate
+    init_y_coord = currentState[1]
+
+    #calculating max displacement possible if continuing on same path with max acceleration
+    max_S = currentState[2]*timestep + 0.5*acceleration_power*timestep*timestep
+
+    #calculating max change in x coordinate
+    max_x_coord = max_S * math.cos(currentState[3])
+
+    #calculating max change in y coordinate
+    max_y_coord = max_S * math.sin(currentState[3])
 
 
 def getStateMatrix():
